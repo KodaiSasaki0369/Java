d0ac1ba65586e3d03b8b485b6dced9692291c0f9
KodaiSasaki0369 <118149998+KodaiSasaki0369@users.noreply.github.com>
2022-11-16 12:24:48 +0900

    Add files via upload

---
+++ Sample/Sample03_04.java
@@ -0,0 +1,14 @@
+package Sample;
+
+public class Sample03_04 {
+
+	public static void main(String[] args) {
+		int number;
+		number = 10;
+		number = 20;
+		number = number + 1;
+		System.out.println(number);
+
+	}
+
+}
