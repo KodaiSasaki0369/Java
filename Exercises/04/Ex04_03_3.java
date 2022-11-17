3dbe52a35c58f9a72fb74e08bd90bc2bc32293db
KodaiSasaki0369 <118149998+KodaiSasaki0369@users.noreply.github.com>
2022-11-17 12:44:22 +0900

    Add files via upload

---
+++ Exercises/04/Ex04_03_3.java
@@ -0,0 +1,20 @@
+package Exercises;
+
+public class Ex04_03_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int a = 20 % 6;
+		int b = 7 % 10;
+		int c = 0 % 4;
+		int d = 20 % 7 % 4;
+		double e = 7 % 5;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+
+}
