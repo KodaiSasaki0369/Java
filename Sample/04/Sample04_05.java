b470d275073da64743abbd7314c76f08ba15d146
KodaiSasaki0369 <118149998+KodaiSasaki0369@users.noreply.github.com>
2022-11-17 12:49:13 +0900

    Add files via upload

---
+++ Sample/04/Sample04_05.java
@@ -0,0 +1,18 @@
+package Sample;
+
+public class Sample04_05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double x = 20, y = 4, a1, a2, a3;
+		
+		a1 = x + y - 3;
+		a2 = x / y * 5;
+		a3 = x / (y * 5);
+		
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+	}
+
+}
---
+++ Sample/04/Sample04_06.java
@@ -0,0 +1,17 @@
+package Sample;
+
+public class Sample04_06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(4 % 3);
+		System.out.println(3 % 3);
+		System.out.println(2 % 3);
+		System.out.println(1 % 3);
+		System.out.println(0 % 3);
+		System.out.println(-1 % 3);
+		System.out.println(-2 % 3);
+		System.out.println(-3 % 3);
+	}
+
+}
