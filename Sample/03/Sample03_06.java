c777c2dc3b297f42508ff66d7466fde7435b1826
KodaiSasaki0369 <118149998+KodaiSasaki0369@users.noreply.github.com>
2022-11-17 12:47:02 +0900

    Add files via upload

---
+++ Sample/03/Sample03_06.java
@@ -0,0 +1,15 @@
+package Sample;
+
+public class Sample03_06 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int n = 10;
+		double x;
+		x = n;
+		x = x + 5;
+		
+		System.out.println(x);
+	}
+
+}
---
+++ Sample/03/Sample03_07.java
@@ -0,0 +1,17 @@
+package Sample;
+
+public class Sample03_07 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		byte b =120;
+		short  s;
+		int    n;
+		long   m;
+		s = b;
+		n = s;
+		m = n;
+		System.out.println(m);
+	}
+
+}
