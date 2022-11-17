12d5d5ca900526b8839102c26c6bfd5382357e30
KodaiSasaki0369 <118149998+KodaiSasaki0369@users.noreply.github.com>
2022-11-17 12:54:39 +0900

    Add files via upload

---
+++ Exercise/04/Ex04_03_2.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class Ex04_03_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		double r = 5.5;
+		double s = r * r * 3.14;
+		
+		System.out.println(s);
+	}
+
+}
