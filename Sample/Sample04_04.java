55c1d883ce500fd3dd18dc0fa6496c2d79dcd666
KodaiSasaki0369 <118149998+KodaiSasaki0369@users.noreply.github.com>
2022-11-17 12:37:18 +0900

    Add files via upload

---
+++ Sample/Sample04_04.java
@@ -0,0 +1,13 @@
+package Sample;
+
+public class Sample04_04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int  n = 12354;
+		char c = (char)n;
+		System.out.println(c);
+		
+	}
+
+}
